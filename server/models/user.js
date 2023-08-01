ac9413ef3e47486ab16d7fa10d76441fea932e7c
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-31 20:20:27 -0400

    corrected user.js model

--- server/models/user.js
+++ server/models/user.js
@@ -1,61 +1,28 @@
-// Create User Schema
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-class User extends Model {
-  checkPassword(enterPassword) {
-    return bcrypt.compareSync(enterPassword, this.password)
-  }
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
+const userSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    //Mae
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
   },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        if (updatedUserData.password) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        }
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-);
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    // reference comment schema
+  ]
+});
+
+const Thought = model('User', userSchema);
 
 module.exports = User;

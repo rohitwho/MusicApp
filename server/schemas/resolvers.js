03a65a755202fc92fc7ca25c675788ab8bb12fbc
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-08-01 20:20:20 -0400

    Adduser/removeuser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,14 @@ const resolvers = {
 
 
         }
+    },
+    Mutation:{
+addUser:async(parent,args)=>{
+    return await User.create(args)
+},
+removeUser:async(parent,args)=>{
+    return await User.findOneAndDelete(args)
+}
     }
 
 }
